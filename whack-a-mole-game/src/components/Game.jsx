a2cf97af8471a915244300aae48e826de0c57639
Nihar Kadam <nihar.kadam@gmail.com>
2024-01-26 00:55:57 +0530

    Added Begin Game button, added configuration

--- whack-a-mole-game/src/components/Game.jsx
+++ whack-a-mole-game/src/components/Game.jsx
@@ -2,14 +2,17 @@ import React, { useEffect, useState } from "react";
 import { Mole } from "./Mole";
 import "../styles/Game.css";
 
+const config = require("../config/config.json");
+
 export const Game = () => {
-  const [score, setScore] = useState(0);
+  const [score, setScore] = useState(config.initialScore);
   const [moles, setMoles] = useState(generateInitialMoles());
-  const [timer, setTimer] = useState(30); // Set the initial timer value in seconds
+  const [timer, setTimer] = useState(config.gameTimer); // Set the initial timer value in seconds
+  const [gameStarted, setGameStarted] = useState(false);
 
   function generateInitialMoles() {
-    return Array.from({ length: 3 }, (_, row) =>
-      Array.from({ length: 3 }, (_, col) => ({
+    return Array.from({ length: config.gridRow }, (_, row) =>
+      Array.from({ length: config.gridCol }, (_, col) => ({
         id: `${row}-${col}`,
         isVisible: false,
       }))
@@ -30,63 +33,75 @@ export const Game = () => {
     }
   };
 
+  const startGame = () => {
+    setGameStarted(true);
+  };
+
   useEffect(() => {
-    const moleInterval = setInterval(() => {
-      const randomRow = Math.floor(Math.random() * 3);
-      const randomCol = Math.floor(Math.random() * 3);
+    if (gameStarted) {
+      const moleInterval = setInterval(() => {
+        const randomRow = Math.floor(Math.random() * config.gridRow);
+        const randomCol = Math.floor(Math.random() * config.gridCol);
 
-      setMoles((prevMoles) => {
-        const updatedMoles = [...prevMoles];
-        updatedMoles[randomRow][randomCol].isVisible = true;
-        return updatedMoles;
-      });
-
-      setTimeout(() => {
         setMoles((prevMoles) => {
-          const resetMoles = [...prevMoles];
-          resetMoles[randomRow][randomCol].isVisible = false;
-          return resetMoles;
+          const updatedMoles = [...prevMoles];
+          updatedMoles[randomRow][randomCol].isVisible = true;
+          return updatedMoles;
         });
+
+        setTimeout(() => {
+          setMoles((prevMoles) => {
+            const resetMoles = [...prevMoles];
+            resetMoles[randomRow][randomCol].isVisible = false;
+            return resetMoles;
+          });
+        }, 1000);
+      }, 300);
+
+      // Clear the moleInterval when the timer reaches 0
+      const timerInterval = setInterval(() => {
+        setTimer((prevTimer) => (prevTimer > 0 ? prevTimer - 1 : 0));
       }, 1000);
-    }, 2000);
 
-    // Clear the moleInterval when the timer reaches 0
-    const timerInterval = setInterval(() => {
-      setTimer((prevTimer) => (prevTimer > 0 ? prevTimer - 1 : 0));
-    }, 1000);
-
-    return () => {
-      clearInterval(moleInterval);
-      clearInterval(timerInterval);
-    };
-  }, []);
+      return () => {
+        clearInterval(moleInterval);
+        clearInterval(timerInterval);
+      };
+    }
+  }, [gameStarted]);
 
   // End the game when the timer reaches 0
   useEffect(() => {
-    if (timer === 0) {
+    if (timer === config.initialScore && gameStarted) {
+      setTimer(config.gameTimer); // Reset the timer for a new game
       alert(`Game Over! Your final score is ${score}`);
-      setScore(0);
+      setScore(config.initialScore);
       setMoles(generateInitialMoles());
-      setTimer(30); // Reset the timer for a new game
+      setGameStarted(false);
     }
-  }, [timer, score]);
+  }, [timer, score, gameStarted]);
 
   return (
-    <div className="main-container">
-      <p className="score-class">Score: {score}</p>
-      <p className="timer-class">Time Left: {timer} seconds</p>
-      <div className="grid-container">
-        {moles.map((row, rowIndex) => (
-          <div key={rowIndex} className="grid-row">
-            {row.map((mole, colIndex) => (
-              <Mole
-                key={mole.id}
-                isVisible={mole.isVisible}
-                onWhack={() => handleWhack(rowIndex, colIndex)}
-              />
-            ))}
-          </div>
-        ))}
+    <div>
+      <div className="text-container">
+        <p className="score-class">Score: {score}</p>
+        <p className="timer-class">Time Left: {timer} seconds</p>
+      </div>
+      <div className="game-container">
+        {!gameStarted && <button onClick={startGame}>Begin Game</button>}
+        <div className="grid-container">
+          {moles.map((row, rowIndex) => (
+            <div key={rowIndex} className="grid-row">
+              {row.map((mole, colIndex) => (
+                <Mole
+                  key={mole.id}
+                  isVisible={mole.isVisible}
+                  onWhack={() => handleWhack(rowIndex, colIndex)}
+                />
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
